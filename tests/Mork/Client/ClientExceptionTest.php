15f4c716328b9553241be09497eff5ee05e57aa4
dr_bonzo <dr_bonzo@northslope.pl>
2012-08-12 21:18:54 +0200

    Mork_Client_ClientExceptionTest

--- tests/Mork/Client/ClientExceptionTest.php
+++ tests/Mork/Client/ClientExceptionTest.php
@@ -1,8 +1,10 @@
 <?php 
 class Mork_Client_ClientExceptionTest extends PHPUnit_Framework_TestCase
 {
-	public function testTODO()
+	public function testExceptionHasRequest()
 	{
-		$this->markTestIncomplete();
+		$exception = new Mork_Client_ClientException(new Mork_Client_Request('fooBar'), 'nothing');
+		
+		$this->assertInstanceOf('Mork_Client_Request', $exception->getRequest());
 	}
 }
